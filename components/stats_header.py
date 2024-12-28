a1e817b499e89f224ab3dc2c121393232cdefbf2
Nathaniel Ledford <nate@nateledford.com>
2024-12-28 10:19:10 -0500

    Move stats header method to module

---
+++ components/stats_header.py
@@ -0,0 +1,36 @@
+from dash import html
+
+from music.tracks import UniversalTracks
+
+
+def stats_header(ut: UniversalTracks) -> html:
+    def tr(children):
+        return html.Tr(children=children, className='group')
+
+    def th(text: str):
+        return html.Th(children=text, className='px-6 py-2 bg-neutral-900/75 group-hover:bg-neutral-900/50')
+
+    def td(text: str):
+        return html.Td(children=text, className='px-6 py-2 group-hover:bg-neutral-700/25')
+
+    return html.Table(
+        className='w-full overflow-hidden bg-neutral-800 border border-neutral-700/30 rounded tabular-nums text-left',
+        children=[
+            tr(children=[
+                th('Total Artists'),
+                td(f'{ut.total_artists}'),
+
+                th('Total Albums'),
+                td(f'{ut.total_albums}'),
+
+                th('Total Tracks'),
+                td(f'{ut.total_tracks}'),
+
+                th('Total Plays'),
+                td(f'{ut.total_plays}'),
+            ]),
+            tr(children=[
+                th('Avg. Plays Per Artist'),
+                td(f'{ut.average_plays_per_artist}'),
+            ])
+        ])
