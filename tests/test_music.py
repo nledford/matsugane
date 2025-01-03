96d82e846624eb5c44c45f083d13bb7f29868e7c
Nathaniel Ledford <nate@nateledford.com>
2025-01-03 15:56:14 -0500

    Fix tests

--- tests/test_music.py
+++ tests/test_music.py
@@ -1,23 +1,31 @@
-from music.album import Album
-from music.artist import Artist
-from music.track import UniversalTrack
+from lastfm_stats.music.album import Album, AlbumName
+from lastfm_stats.music.artist import Artist, ArtistName
+from lastfm_stats.music.track import PlayedAt, TrackTitle, UniversalTrack
 
 test_timestamp = '1494824400'
 
-test_artist = Artist(name="Rush")
-test_album = Album(name="Moving Pictures", artist=test_artist)
-test_track = UniversalTrack(title="Limelight", artist=test_artist, album=test_album, plays=1, played_at=test_timestamp)
+test_artist = Artist(name=ArtistName("Rush"))
+test_album = Album(name=AlbumName("Moving Pictures"), artist=test_artist)
+test_track = UniversalTrack(title=TrackTitle("Limelight"),
+                            artist=test_artist,
+                            album=test_album,
+                            plays=1,
+                            played_at=PlayedAt(test_timestamp))
+
 
 def test_track_title():
     assert test_track.title == "Limelight"
 
+
 def test_album_name():
     assert test_track.album.name == "Moving Pictures"
+
 
 def test_album_sort_name():
     assert test_album.sort_name == "moving pictures"
     assert test_album.sort_name != "Moving pictures"
     assert test_album.sort_name != "moving picture"
+
 
 def test_album_artist():
     assert test_album.artist.name == 'Rush'
