261cd0ec3b6d3b54e8453111b9c24511c3d6cfa5
Nathaniel Ledford <nate@nateledford.com>
2025-01-07 08:20:42 -0500

    Add last refresh time to stats header

--- src/lastfm_stats/components/stats_header.py
+++ src/lastfm_stats/components/stats_header.py
@@ -1,9 +1,20 @@
+from datetime import datetime
+
 from dash import html
 
 from lastfm_stats.music.tracks import UniversalTracks
 
 
-def stats_header(ut: UniversalTracks) -> html.Table:
+def last_updated() -> html.Div:
+    now = datetime.now().time().strftime('%H:%M:%S')
+
+    return html.Div(
+        className='text-xs mb-1.5',
+        children=[f'Last Updated: {now}']
+    )
+
+
+def stats_header(ut: UniversalTracks) -> html.Div:
     def tr(children):
         return html.Tr(children=children, className='group')
 
@@ -13,24 +24,29 @@ def stats_header(ut: UniversalTracks) -> html.Table:
     def td(text: str):
         return html.Td(children=text, className='px-6 py-2 group-hover:bg-neutral-700/25')
 
-    return html.Table(
-        className='w-full overflow-hidden bg-neutral-800 border border-neutral-700/30 rounded tabular-nums text-left',
-        children=[
-            tr(children=[
-                th('Total Artists'),
-                td(f'{ut.total_artists}'),
+    sh = html.Div(children=[
+        last_updated(),
+        html.Table(
+            className='w-full overflow-hidden bg-neutral-800 border border-neutral-700/30 rounded tabular-nums text-left',
+            children=[
+                tr(children=[
+                    th('Total Artists'),
+                    td(f'{ut.total_artists}'),
 
-                th('Total Albums'),
-                td(f'{ut.total_albums}'),
+                    th('Total Albums'),
+                    td(f'{ut.total_albums}'),
 
-                th('Total Tracks'),
-                td(f'{ut.total_tracks}'),
+                    th('Total Tracks'),
+                    td(f'{ut.total_tracks}'),
 
-                th('Total Plays'),
-                td(f'{ut.total_plays}'),
-            ]),
-            tr(children=[
-                th('Avg. Plays Per Artist'),
-                td(f'{ut.average_plays_per_artist}'),
+                    th('Total Plays'),
+                    td(f'{ut.total_plays}'),
+                ]),
+                tr(children=[
+                    th('Avg. Plays Per Artist'),
+                    td(f'{ut.average_plays_per_artist}'),
+                ])
             ])
-        ])
+    ])
+
+    return sh
