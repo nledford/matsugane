71a8190d07fc4357fdad163ad181fdcac8340c97
Nathaniel Ledford <nate@nateledford.com>
2025-01-31 13:18:01 -0500

    Last refresh refactoring

--- src/matsugane/main.py
+++ src/matsugane/main.py
@@ -13,6 +13,10 @@ def current_time() -> str:
     return datetime.now().strftime("%H:%M:%S")
 
 
+def get_last_refresh() -> str:
+    return f"Last Refresh: {current_time()}"
+
+
 class MatsuganeApp(App):
     """A Textual app to display stats from Lastfm."""
 
@@ -20,15 +24,16 @@ class MatsuganeApp(App):
     BINDINGS = [("r", "refresh_data", "Refresh Last.fm Data")]
 
     tracks: reactive[UniversalTracks] = reactive(UniversalTracks(), recompose=True)
-    last_refresh: reactive[str] = reactive(current_time())
+    last_refresh: reactive[str] = reactive(get_last_refresh())
 
     def __init__(self) -> None:
         super().__init__()
-        self.set_reactive(MatsuganeApp.last_refresh, current_time())
+        self.set_reactive(MatsuganeApp.last_refresh, get_last_refresh())
 
     def update_last_refresh(self):
-        self.last_refresh = f"Last Refresh: {current_time()}"
+        self.last_refresh = get_last_refresh()
 
+    # TODO set last refresh on app load
     def watch_last_refresh(self, text: str) -> None:
         self.query_one("#lastRefresh", Label).update(text)
 
@@ -39,7 +44,7 @@ class MatsuganeApp(App):
         """Create child widgets for the app."""
         yield Header()
 
-        yield Label(f"Last Refresh: {current_time()}", id="lastRefresh")
+        yield Label(self.last_refresh, id="lastRefresh")
         yield StatsHeader().data_bind(MatsuganeApp.tracks)
 
         yield Footer()
