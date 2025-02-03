986e303eb5e03d78c8e7ef4dc2ace7099b9bb325
Nathaniel Ledford <nate@nateledford.com>
2025-02-03 08:11:43 -0500

    Create stretchy data table component

--- src/matsugane/components/recent_plays.py
+++ src/matsugane/components/recent_plays.py
@@ -1,10 +1,10 @@
-from textual import events
 from textual.app import ComposeResult
 from textual.reactive import reactive
 from textual.widget import Widget
-from textual.widgets import DataTable, Label
+from textual.widgets import Label
 
 from matsugane import utils
+from matsugane.components.ui import StretchyDataTable
 from matsugane.music.tracks import UniversalTracks
 
 
@@ -16,22 +16,8 @@ class RecentPlays(Widget):
         yield RecentPlaysTable().data_bind(RecentPlays.ut)
 
 
-class RecentPlaysTable(DataTable):
+class RecentPlaysTable(StretchyDataTable):
     ut: reactive[UniversalTracks] = reactive(UniversalTracks(), recompose=True)
-
-    def on_resize(self, event: events.Resize) -> None:
-        """
-        Automatically expands or shrinks table columns to fit available space
-        SOURCE: https://github.com/Textualize/textual/issues/5455#issuecomment-2571464847
-        """
-        total_width = event.size.width
-        total_padding = 2 * (self.cell_padding * len(self.columns))
-        column_width = (total_width - total_padding) // len(self.columns)
-
-        for column in self.columns.values():
-            column.auto_width = False
-            column.width = column_width
-        self.refresh()
 
     def watch_ut(self, tracks: UniversalTracks) -> None:
         self.ut = tracks
@@ -47,7 +33,7 @@ class RecentPlaysTable(DataTable):
         self.clear()
 
         if ut.is_empty and self.row_count == 0:
-            self.add_row("No tracks", "", "", "", key="NO DATA")
+            self.add_row("No tracks", "", "", "", key="NO DATA RECENT PLAYS")
         else:
             for track in ut.lastfm_tracks:
                 self.add_row(
---
+++ src/matsugane/components/ui.py
@@ -0,0 +1,19 @@
+from textual import events
+from textual.widgets import DataTable
+
+
+class StretchyDataTable(DataTable):
+    """
+    Automatically expands or shrinks table columns to fit available space
+    SOURCE: https://github.com/Textualize/textual/issues/5455#issuecomment-2571464847
+    """
+
+    def on_resize(self, event: events.Resize) -> None:
+        total_width = event.size.width
+        total_padding = 2 * (self.cell_padding * len(self.columns))
+        column_width = (total_width - total_padding) // len(self.columns)
+
+        for column in self.columns.values():
+            column.auto_width = False
+            column.width = column_width
+        self.refresh()
