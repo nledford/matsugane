e35a98c5c41f8a55c309235a7d67de8e65189545
Nathaniel Ledford <nate@nateledford.com>
2025-02-27 10:52:24 -0500

    Fix issues with tracks class

--- matsugane/Lastfm/Track.cs
+++ matsugane/Lastfm/Track.cs
@@ -1,23 +1,29 @@
+// ReSharper disable MemberCanBePrivate.Global
+
+// ReSharper disable UnusedMember.Global
+
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+
 namespace matsugane.Lastfm;
 
 public class Track
 {
     public string TrackId => $"track|{SortTitle}|{SortArtist}|{SortAlbum}";
     public string PlayId => $"play|{PlayedAt}";
-    public string Title { get; private init; }
-    public string SortTitle { get; private init; }
+    public string Title { get; private init; } = string.Empty;
+    public string SortTitle { get; private init; } = string.Empty;
     public string ArtistId => $"artist|{SortArtist}";
-    public string Artist { get; private init; }
-    public string SortArtist { get; private init; }
+    public string Artist { get; private init; } = string.Empty;
+    public string SortArtist { get; private init; } = string.Empty;
     public string AlbumId => $"album|{SortArtist}|{SortAlbum}";
-    public string Album { get; private init; }
-    public string SortAlbum { get; private init; }
+    public string Album { get; private init; } = string.Empty;
+    public string SortAlbum { get; private init; } = string.Empty;
     public DateTime PlayedAt { get; private init; }
     public string PlayedAtFmt => PlayedAt.ToString("T");
 
     public static async Task<Track> BuildTrack(string title, string artist, string album, string playedAt)
     {
-        return new Track()
+        return new Track
         {
             Title = title,
             SortTitle = await Utils.BuildSortNameAsync(title),
@@ -25,7 +31,7 @@ public class Track
             SortArtist = await Utils.BuildSortNameAsync(artist),
             Album = album,
             SortAlbum = await Utils.BuildSortNameAsync(album),
-            PlayedAt = DateTimeOffset.FromUnixTimeSeconds(long.Parse(playedAt)).LocalDateTime,
+            PlayedAt = DateTimeOffset.FromUnixTimeSeconds(long.Parse(playedAt)).LocalDateTime
         };
     }
 }
