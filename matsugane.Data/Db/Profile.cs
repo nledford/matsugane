0c3084ccbe3f69aa154301c77a05f4def61a2db1
Nathaniel Ledford <nate@nateledford.com>
2025-03-06 09:57:58 -0500

    Adjust profiles

--- matsugane.Data/Db/Profile.cs
+++ matsugane.Data/Db/Profile.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace matsugane.Data.Db;
@@ -5,7 +6,7 @@ namespace matsugane.Data.Db;
 [Table("profile")]
 public class Profile
 {
-    [Column("profile_id")] public required int ProfileId { get; set; }
+    [Key] [Column("profile_id")] public required int ProfileId { get; set; }
 
     [Column("playlist_id")] public required string PlaylistId { get; set; }
 
@@ -24,6 +25,7 @@ public class Profile
     [Column("time_limit")] public required int TimeLimit { get; set; }
 
     [Column("track_limit")] public required int TrackLimit { get; set; }
+
 }
 
 [Table("v_profile")]
@@ -41,3 +43,33 @@ public class VProfile : Profile
 
     [Column("eligible_for_refresh")] public required bool EligibleForRefresh { get; set; }
 }
+
+// [Table("profile_section")]
+// public class ProfileSection
+// {
+//
+//     [Key] [Column("profile_section_id")] public required int ProfileSectionId { get; set; }
+//
+//     [Column("profile_id")]
+//     [ForeignKey("Profile")]
+//     public required int ProfileId { get; set; }
+//
+//     public virtual required Profile Profile { get; set; }
+//
+//     [Column("section_type")] public required string SectionType { get; set; }
+//
+//     [Column("enabled")] public bool Required { get; set; }
+//
+//     [Column("deduplicate_tracks_by_guid")] public bool DeduplicateTracksByGuid { get; set; }
+//
+//     [Column("deduplicate_tracks_by_title_and_artist")]
+//     public bool DeduplicateTracksByTitleAndArtist { get; set; }
+//
+//     [Column("maximum_tracks_by_artists")] public int MaximumTracksByArtists { get; set; }
+//
+//     [Column("minimum_track_rating")] public int MinimumTrackRating { get; set; }
+//
+//     [Column("randomize_tracks")] public bool RandomizeTracks { get; set; }
+//
+//     [Column("sorting")] public required string Sorting { get; set; }
+// }
